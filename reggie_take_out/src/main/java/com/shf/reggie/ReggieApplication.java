990ed76d17dccf839b3aa3f9fdf558a655009adf
shuhongfan <shuhongfan@foxmail.com>
2022-04-24 11:14:56 +0800

    init

---
+++ reggie_take_out/src/main/java/com/shf/reggie/ReggieApplication.java
@@ -0,0 +1,17 @@
+package com.shf.reggie;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author shuho
+ */
+@SpringBootApplication
+@Slf4j
+public class ReggieApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReggieApplication.class,args);
+        log.info("项目启动成功");
+    }
+}
---
+++ reggie_take_out/src/main/java/com/shf/reggie/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,11 @@
+package com.shf.reggie.service.impl;
+
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.shf.reggie.entity.Employee;
+import com.shf.reggie.mapper.EmployeeMapper;
+import com.shf.reggie.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeServiceImpl extends ServiceImpl<EmployeeMapper, Employee> implements EmployeeService {
+}
